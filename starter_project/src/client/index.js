ff68671e6da156e17b10a363e990e7333b9d040c
Jangulo7 <johanna.angulo@gmail.com>
2024-06-17 10:12:51 -0700

    Defined output in dev, defined export in index.js and imported styles.

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -1,9 +1,11 @@
 // js files
-import { handleSubmit } from './js/formHandler'
+import { handleSubmit } from "./js/formHandler"
+import { checkForName } from "./js/nameChecker"
+
+console.log(checkForName);
 
 
-
-// alert("I EXIST")
+alert("I EXIST")
 // console.log("CHANGE!!");
 
 // sass files
